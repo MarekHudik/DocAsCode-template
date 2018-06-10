ea8af75eb3df24c49ff588cfcc18b59eec518956
Marek Hudik <hudik.marek@gmail.com>
2018-06-11 01:02:59 +0200

    webpack continue

--- twitter-bootstrap/src/empty.js
+++ twitter-bootstrap/src/empty.js
@@ -1,1 +1,2 @@
-
+// import the CSS we want to use here
+import './bootstrap.min.css';
--- twitter-bootstrap/webpack.config.js
+++ twitter-bootstrap/webpack.config.js
@@ -1,24 +1,35 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 const webpack = require('webpack'); //to access built-in plugins
 const path = require('path');
 
 const config = {
-  entry: './src/pandoc-default-html5.html',
+  mode: 'development',
+  entry: {
+    empty: './src/empty.js',
+    // css: './src/bootstrap.min.css'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'twitter-bootstrap.bundle.html'
+    // filename: 'empty.js'
+    filename: '[name].js',
   },
+  // devtool: "source-map",
   module: {
     rules: [
-      { test: /\.txt$/, use: 'raw-loader' }
+      { test: /\.(html)$/, use: {loader: 'html-loader',}},
+      { test: /\.txt$/, use: 'raw-loader' },
+      { test: /\.css$/, use: ['to-string-loader', 'css-loader'],
+      }
     ]
   },
   plugins: [
-    // new HtmlWebpackPlugin({
-    //   template: './src/pandoc-default-html5.html'
-    // })
-    new ExtractTextPlugin('index.html')
+    new HtmlWebpackPlugin({
+      template: './src/pandoc-default-html5.html',
+      filename: 'twitter-bootstrap.bundle.html',
+      inlineSource: '.(css)$'
+    }),
+    new HtmlWebpackInlineSourcePlugin()
   ]
 };
 
